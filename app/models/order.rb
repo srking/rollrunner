e58ee2f62d7eb46e9dc153f0436b3844bcf3fe6d
Steven King <steven.king@kotikan.com>
2015-07-02 19:04:40 +0100

    Destroy dependent rolls when deleting an order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ActiveRecord::Base
   belongs_to :domain
   belongs_to :owner, class_name: User
-  has_many :rolls
+  has_many :rolls, dependent: :destroy
 
   validates_presence_of :name
 end
