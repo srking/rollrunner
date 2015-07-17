40356a8be8dc1ed5e548f5a8ec55223759e79a22
Steven King <steven.king@kotikan.com>
2015-07-18 00:17:55 +0100

    Add 'Cafe' model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150627165609) do
+ActiveRecord::Schema.define(version: 20150717231353) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cafes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "domains", force: :cascade do |t|
     t.string   "name"
